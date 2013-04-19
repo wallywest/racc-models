dfb8a6606e073430bab0c7fc3357319410a33ebd
Justin Erny <jerny@vailsys.com>
2013-04-19 16:26:53 -0500

    add configs to yml files

--- lib/racc/config.rb
+++ lib/racc/config.rb
@@ -1,19 +1,4 @@
 class Racc::App
   module Config
-    def self.alpha
-      {:adapter => 'tinytds', 
-       :host =>'sdwsql01', 
-       :database => 'racc_v2b_alpha', 
-       :username => 'racc_v2b_alpha', 
-       :password => 'racc_v2b_alpha8245'}
-    end
-
-    def self.local
-      {:adapter => 'postgresql', 
-       :host =>'localhost', 
-       :database => 'racc_dev', 
-       :username => 'root', 
-       :password => ''}
-    end
   end
 end
